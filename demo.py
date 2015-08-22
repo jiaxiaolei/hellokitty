87138c1fadbd046e7883c6b0257c540e8a8d976b
wangyiyang <30339245@qq.com>
2015-08-22 08:14:58 +0800

    update

--- demo.py
+++ demo.py
@@ -4,15 +4,17 @@ import re
 from bs4 import BeautifulSoup
 
 headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'  ,
-                        'Referer':'http://www.zhihu.com/articles' }   
-request = urllib2.Request(url = "http://macshuo.com/",headers = headers)
+                        'Referer':'http://www.zhihu.com/articles' }
+print "headers success"   
+request = urllib2.Request(url = "http://www.torrentkitty.org/search/",headers = headers)
 response = urllib2.urlopen(request)
 content =  response.read()
-
-soup = BeautifulSoup(content,"html5lib")
-found = BeautifulSoup(str(soup.find_all(id="archives-2")[0]))
-result = found.find_all("a")
+print "get success"
+soup = BeautifulSoup(content)
+result = soup.find_all(href=re.compile("/search/"))
+# result = found.find_all("a")
 for link in result:
-	print "a"
 	print link.get('href')
+
+
 

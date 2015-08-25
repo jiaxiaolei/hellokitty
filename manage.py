38abf14e698a24135f3ebe8c78310586cd5a37e5
翊仰 <wangyiyang@users.noreply.github.com>
2015-08-25 14:18:21 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,34 +1,10 @@
-server {
+#!/usr/bin/env python
+import os
+import sys
 
-    listen  80;
-    server_name hellokitty.wangyiyang.cn;
-    access_log /home/kk/www/hellokitty/logs/access.log;
-    error_log /home/kk/www/hellokitty/logs/error.log;
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellokitty.settings")
 
-    #charset koi8-r;
+    from django.core.management import execute_from_command_line
 
-    #access_log  logs/host.access.log  main;
-
-    location / {
-     include        uwsgi_params;
-     uwsgi_pass     127.0.0.1:8074;
-    }
-
-    #error_page  404              /404.html;
-
-    # redirect server error pages to the static page /50x.html
-    #
-    error_page   500 502 503 504  /50x.html;
-    location = /50x.html {
-        root   html;
-    }
-
-    location /static/ {
-        alias  /home/kk/www/hellokitty/hellokitty/static/;
-        index  index.html index.htm;
-    }
-
-    location /media/ {
-        alias  /home/kk/www/hellokitty/hellokitty/media/;
-    }
-}
+    execute_from_command_line(sys.argv)
